1bf793ebd594cde3277b4e6896a8a3021f9fe838
Youssef Dawoud <you.dawoud@gmail.com>
2025-06-17 23:03:45 +0200

    Remove redundant makedirs

--- WorkSpace.py
+++ WorkSpace.py
@@ -3,13 +3,9 @@ import shutil
 
 class ManageWorkSpace:
 
-    def __init__(self,root_dir=os.getcwd(),datasets=None):
-        self.root_dir = root_dir
-        self.default_dir_dict = {'datasets_dir':['/Datasets/Raw/',
-                                                 '/Datasets/FewShot/Source/','/Datasets/FewShot/Target/'],
-                                 'datasets':['/Datasets/Raw/'+dataset+'/' for dataset in datasets],
-                                 'image_dir':['/Datasets/Raw/'+dataset+'/Image/' for dataset in datasets],
-                                 'gt_dir':['/Datasets/Raw/'+dataset+'/Groundtruth/' for dataset in datasets],
+    def __init__(self,root_dir=None,datasets=None):
+        self.root_dir = root_dir if root_dir else os.getcwd()
+        self.default_dir_dict = {'datasets_dir':['/Datasets/FewShot/Source/','/Datasets/FewShot/Target/'],
                                  'Logging':['/Logging/Meta-models/','/Logging/Supervised-models/'],
                                  'models':['/models/Meta-models/','/models/Supervised-models/']}
 
@@ -30,20 +26,15 @@ class ManageWorkSpace:
                 if os.path.exists(dir):
                     shutil.rmtree(dir)
 
-    def create_dir(self,dirs:list):
-        for dir in dirs:
-            if not os.path.exists(dir):
-                os.makedirs(dir)
+    def create_dir(self,dir):
+        if not os.path.exists(dir):
+            os.makedirs(dir, exist_ok=True)
 
     #Create Default Workspace Directories
     def create_default(self):
         for dirs in self.default_dir_dict.keys():
             for dir in self.default_dir_dict[dirs]:
-                self.create_dir([self.root_dir+dir])
-
-
-
-
+                self.create_dir(self.root_dir+"/"+dir)
 
 if __name__ == '__main__':
     datasets = ['B5','B39','ssTEM','EM','TNBC']
